e48c8b8131cbc6925061af8ddd36ff24763bdee8
stasfly <s19f84@gmail.com>
2022-03-11 22:25:49 +0200

    ch.5. Finish with layouts and routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,24 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root             'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
 
-# reke test and rake test:integration go only with both sets of get.
+#reke test goes only wit upper set of get.
+#rake test:integration goes only with bottom set of get.
+
 # link_to seems doesn't respond to a name_path pettern. 
   #get 'home'  => 'static_pages#home'
   
-  get 'help'  => 'static_pages#help'
-  get 'about',  to: 'static_pages#about'
+  get 'help'     =>  'static_pages#help'
+  get 'about',   to: 'static_pages#about'
   get 'contact', to: 'static_pages#contact'
+  get 'signup',  to: 'users#new' #link_to works only wit 'signup' or '/signup'
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   #resources :static_pages
   # Defines the root path route ("/")
