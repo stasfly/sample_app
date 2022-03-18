98fea2e73dcad1d88d6616beb67a0da22f9c2222
stasfly <s19f84@gmail.com>
2022-03-18 22:26:40 +0200

    Finish user signup.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
+  
   def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+    #debugger
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcom, dear #{@user[:name]}, to Sample App"
+      redirect_to @user      # some code
+    else
+      #redirect_to action: :new
+      render "new"# doesn't work properly.
+    end 
+  end
+
+
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   get 'users/new'
+  #get 'users/show'
   root             'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
+
+  
 
 #reke test goes only wit upper set of get.
 #rake test:integration goes only with bottom set of get.
@@ -16,7 +19,7 @@ Rails.application.routes.draw do
   get 'about',   to: 'static_pages#about'
   get 'contact', to: 'static_pages#contact'
   get 'signup',  to: 'users#new' #link_to works only wit 'signup' or '/signup'
-
+  resources :users
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
